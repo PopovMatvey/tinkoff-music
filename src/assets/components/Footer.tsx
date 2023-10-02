075e261453e953b4dc4be3693320881eb15df7bc
матвей попов <matyha550@gmail.com>
2023-10-02 09:51:44 +0300

    release v 2.0.1

--- src/assets/components/Footer.tsx
+++ src/assets/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from '../images/tinkoff-music-logo.png';
+import logo from '../images/music-logo.jpg';
 import "../css/Footer.css";
 
 export function Footer() {
@@ -8,6 +8,7 @@ export function Footer() {
         <footer>
             <div className="footer-content">
                 <img src={logo} className="footer-logo" alt="logo" />
+                <h1>Music platform</h1>
             </div>
             <span>Все права защищены © 2023</span>
         </footer>
--- src/assets/components/HeaderInformation.tsx
+++ src/assets/components/HeaderInformation.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import logo from '../images/tinkoff-music-logo.png';
+import logo from '../images/music-logo.jpg';
 import '../css/HeaderInformation.css';
 
 export function HeaderInformation() {
     return (
         <div className="header-information">
             <img src={logo} className="header-logo" alt="logo" />
+            <h1>Music platform</h1>
         </div>
     );
 }
